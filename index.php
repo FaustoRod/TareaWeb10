13e3a97761e274937d2a4c8ff299c0789b20ffd1
FaustoRod <faustoelnike@gmail.com>
2017-03-22 18:12:59 -0400

    Changes

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>ReTech</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	
+	<body>
+		<header>
+						
+			<div class="header-item logo-cont">
+				<div id="logo">
+					<img src="assets/techlogo.png" style="width:100%;height:100%;">
+				</div>
+				
+				<div id="titulo">
+				d
+				</div>
+			</div>
+			
+			<div class="header-item">
+			</div>
+			
+			<div class="header-item">
+				redes
+			</div>
+		
+		</head>
+		<script type="text/javascript" src="JavaScript/javascript.js"></script>
+		<script type="text/javascript" src="JavaScript/jquery-3.1.1.min.js"></script>
+	</body>
+
+</html>
---
+++ views/viewall.php
@@ -0,0 +1,18 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>ReTech</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	
+	<body>
+	
+		<script type="text/javascript" src="JavaScript/javascript.js"></script>
+		<script type="text/javascript" src="JavaScript/jquery-3.1.1.min.js"></script>
+	</body>
+
+</html>
